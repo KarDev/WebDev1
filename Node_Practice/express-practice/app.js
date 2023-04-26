9fe57b14e7b8364ff8e14ca91d4f7dc3f63fba0d
Karan <featkaran890@gmail.com>
2023-04-26 18:36:07 +0530

    updates

--- Node_Practice/express-practice/app.js
+++ Node_Practice/express-practice/app.js
@@ -1,36 +1,24 @@
 const path = require('path');
 
 const express = require('express');
-// const bodyParser = require('body-parser');
-
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
-
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-// app.use(bodyParser.urlencoded({extended: false}));
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
-
-// app.use('/',(req, res, next) => {
-//     console.log("This always runs.");
-//     next();
-// });
-
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404);
-    res.sendFile(path.join(__dirname, 'views','404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
-
-// module.exports = path.dirname(require.main.filename);
